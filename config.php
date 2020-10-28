628969dd24e9dd80c1b23a5a6cbf3db8d57d9414
zainsarfraz <44726141+zainsarfraz@users.noreply.github.com>
2020-10-28 12:57:41 +0500

    Update config.php
    live db info updated

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-    $servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "cars";
+    $servername = "enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+	$username = "jdun7b1t2furrvu0";
+	$password = "nwa0f5uc6ey7vvt7";
+	$dbname = "ofzx91jtnssv5hkm";
 ?>
